60fe2ad50c07698ede4b622cc0e06b1fba3a0030
gagelo01 <eloi.gagnon.1@ulaval.ca>
2023-04-17 16:33:45 -0400

    include the convert_outcome_to_exposure function2

--- R/general_mr.R
+++ R/general_mr.R
@@ -49,10 +49,36 @@ convert_exposure_to_outcome <- function (exposure_dat) {
   outcome_dat <- TwoSampleMR::format_data(exposure_dat, beta_col = "beta.exposure", type = "outcome",
                                           se_col = "se.exposure", pval_col = "pval.exposure", phenotype_col = "exposure",
                                           effect_allele_col = "effect_allele.exposure", other_allele_col = "other_allele.exposure",
-                                          eaf_col = "eaf.exposure", units_col = "units.exposure")
+                                          eaf_col = "eaf.exposure", units_col = "units.exposure", ncase_col = "ncase.exposure",
+                                          ncontrol_col = "ncontrol.exposure",
+                                          samplesize_col = "samplesize.exposure",
+                                          gene_col = "gene.exposure",
+                                          id_col = "id.exposure")
   outcome_dat <- base::merge(outcome_dat, id, by.x = "outcome", by.y = "exposure")
   outcome_dat <- subset(outcome_dat, select = -c(id.outcome))
   names(outcome_dat)[names(outcome_dat) == "id.exposure"] <- "id.outcome"
   return(outcome_dat)
 }
 
+
+#' In TwoSampleMR format convert outcome to exposure
+#'
+#' @param outcome_dat a TwoSampleMR data.frame
+#'
+#' @return
+#' @export
+
+convert_outcome_to_exposure <- function(outcome_dat) {
+  id <- subset(outcome_dat, !duplicated(outcome), select = c(outcome,
+                                                             id.outcome))
+  outcome_dat <- TwoSampleMR::format_data(outcome_dat, beta_col = "beta.outcome",
+                                          type = "exposure", se_col = "se.outcome", pval_col = "pval.outcome",
+                                          phenotype_col = "outcome", effect_allele_col = "effect_allele.outcome",
+                                          other_allele_col = "other_allele.outcome", eaf_col = "eaf.outcome",
+                                          units_col = "units.outcome", ncase_col = "ncase.outcome",
+                                          ncontrol_col = "ncontrol.outcome", samplesize_col = "samplesize.outcome",
+                                          gene_col = "gene.outcome", id_col = "id.outcome")
+  outcome_dat <- base::merge(outcome_dat, id, by.x = c("id.exposure", "exposure"),
+                             by.y = c("id.outcome", "outcome"))
+  return(outcome_dat)
+}
