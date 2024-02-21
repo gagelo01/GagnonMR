d2815c41c38699a60ca3a7e5669e31e6e6a400bc
gagelo01 <eloi.gagnon.1@ulaval.ca>
2024-02-21 09:28:59 -0500

    create wald_quick

--- R/sensitivity_mr_analysis.R
+++ R/sensitivity_mr_analysis.R
@@ -34,12 +34,14 @@ mr_conmix_wrapper <- function (dat) {
 #' The "random_underdispersion" is the default method in TwoSampleMR.
 #' @param short if TRUE only runs egger, weighted mode median and egger if FALSE runs the rest
 #' @param skip_presso default FALSE. will run MR presso. If True won't run MR presso. MR presso takes load of memory.
+#' @param tsmr_robust will be passed on the TwoSampleMR::mr function
 #' @return a data.frame with results of sensitivity analysis.
 #'
 #' @import data.table
 #'
 #' @export
-all_mr_methods <- function(dat, SignifThreshold = 0.05, Primary_mr = "default", short = FALSE, skip_presso = FALSE) {
+all_mr_methods <- function(dat, SignifThreshold = 0.05, Primary_mr = "default", short = FALSE, skip_presso = FALSE,
+                           tsmr_robust = c("mr_weighted_mode", "mr_weighted_median", "mr_egger_regression")) {
   dat<-dat[dat$mr_keep == TRUE,]
   if (nrow(dat) == 1) {
     res <- TwoSampleMR::mr(dat, method_list = c("mr_wald_ratio"))
@@ -63,7 +65,7 @@ all_mr_methods <- function(dat, SignifThreshold = 0.05, Primary_mr = "default", 
       }
     }
     dat<- dat[dat$mr_keep,]
-    res <- TwoSampleMR::mr(dat, method_list=c(primary_analysis, "mr_weighted_mode", "mr_weighted_median", "mr_egger_regression"))
+    res <- TwoSampleMR::mr(dat, method_list=c(primary_analysis, tsmr_robust))
 
     if(short == TRUE) {return(res)}
     #mr raps
@@ -161,4 +163,84 @@ all_mr_methods <- function(dat, SignifThreshold = 0.05, Primary_mr = "default", 
     result$type_of_test <- type_of_test
     return(result)
   }
+}
+
+
+#' Perform a wald ratio
+#'
+#' @param inst_all_sign_clump_arg the instrument object
+#' @param all_outcome_arg the outcome object
+#' @param id_exposure_name the id.exposure of the exposure
+#' @param id_outcome_name the id.outcome of the outcome
+#'
+#' @return
+#' @export
+
+wald_quick <- function(inst_all_sign_clump_arg, all_outcome_arg, id_exposure_name, id_outcome_name = ".*") {
+
+  message(paste0("###### Analysing ", id_exposure_name, " #########"))
+
+  harm_univariate <- TwoSampleMR::harmonise_data(exposure_dat = inst_all_sign_clump_arg[grep(paste0("^" ,id_exposure_name, "$"), id.exposure),],
+                                                 outcome_dat = all_outcome_arg[ grep(paste0("^" ,id_outcome_name, "$"), id.outcome),],
+                                                 action = 1)
+  harm_univariate <- TwoSampleMR::steiger_filtering(harm_univariate)
+  data.table::setDT(harm_univariate)
+  harm_univariate <- harm_univariate[mr_keep==TRUE,]
+  if (dim(harm_univariate)[1] == 0) {
+    return(cbind(inst_all_sign_clump_arg[grep(paste0("^" ,id_exposure_name, "$"), id.exposure), .(id.exposure,exposure)],
+                 dplyr::distinct(all_outcome_arg[grep(paste0("^" ,id_outcome_name, "$"), id.outcome), .(id.outcome,outcome)])))
+  }
+  res <- TwoSampleMR::mr(harm_univariate, method_list = c("mr_wald_ratio"))
+  res$nsnp <- harm_univariate$SNP
+  res$type_of_test <- "Primary analysis"
+  data.table::setDT(res)
+  res$lci<-(res$b)-(res$se*1.96)
+  res$uci <- (res$b)+(res$se*1.96)
+  harm_univariate <- TwoSampleMR::add_rsq(harm_univariate) %>% data.table::as.data.table(.)
+  res$fstat<-GagnonMR::fstat_fromdat(split(harm_univariate, 1:nrow(harm_univariate)))
+  res$rsq <- harm_univariate$rsq.exposure
+  res <- merge(res, harm_univariate[,.(id.exposure,id.outcome,steiger_dir,steiger_pval)], by = c("id.exposure", "id.outcome"))
+  data.table::setDT(res)
+  return(res)
+}
+
+
+#' Perform a wald ratio analyses and format the data
+#'
+#' @param inst_all_sign_clump_arg The instrument data.table
+#' @param all_outcome_arg The all_outcome data.table
+#' @param id_exposure_name A regular expression to select the id.exposure
+#' @param id_outcome_name A regular expression to select the id.outcome
+#'
+#' @return
+#' @export
+
+wald_quick <- function(inst_all_sign_clump_arg, all_outcome_arg, id_exposure_name, id_outcome_name = ".*") {
+
+  message(paste0("###### Analysing ", id_exposure_name, " #########"))
+  stopifnot(inst_all_sign_clump_arg[grep(paste0("^" ,id_exposure_name, "$"), id.exposure),.N]==1)
+  harm_univariate <- TwoSampleMR::harmonise_data(exposure_dat = inst_all_sign_clump_arg[grep(paste0("^" ,id_exposure_name, "$"), id.exposure),],
+                                                 outcome_dat = all_outcome_arg[ grep(paste0("^" ,id_outcome_name, "$"), id.outcome),],
+                                                 action = 1)
+
+  if (dim(harm_univariate)[1] == 0) {
+    return(cbind(inst_all_sign_clump_arg[grep(paste0("^" ,id_exposure_name, "$"), id.exposure), .(id.exposure,exposure)],
+                 dplyr::distinct(all_outcome_arg[grep(paste0("^" ,id_outcome_name, "$"), id.outcome), .(id.outcome,outcome)])))
+  }
+
+  harm_univariate <- TwoSampleMR::steiger_filtering(harm_univariate)
+  data.table::setDT(harm_univariate)
+  harm_univariate <- harm_univariate[mr_keep==TRUE,]
+  res <- TwoSampleMR::mr(harm_univariate, method_list = c("mr_wald_ratio"))
+  res$nsnp <- harm_univariate$SNP
+  res$type_of_test <- "Primary analysis"
+  data.table::setDT(res)
+  res$lci<-(res$b)-(res$se*1.96)
+  res$uci <- (res$b)+(res$se*1.96)
+  harm_univariate <- TwoSampleMR::add_rsq(harm_univariate) %>% data.table::as.data.table(.)
+  res$fstat<-GagnonMR::fstat_fromdat(split(harm_univariate, 1:nrow(harm_univariate)))
+  res$rsq <- harm_univariate$rsq.exposure
+  res <- merge(res, harm_univariate[,.(id.exposure,id.outcome,steiger_dir,steiger_pval)], by = c("id.exposure", "id.outcome"))
+  data.table::setDT(res)
+  return(res)
 }
